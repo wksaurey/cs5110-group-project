10ee0984f48828d7f490ec7e8ddaae7709714a8f
Rox <roxseeley@gmail.com>
2025-11-21 14:10:37 -0700

    Created base for centralized model

---
+++ CentralizedModel.py
@@ -0,0 +1,15 @@
+import DecisionModel
+
+class Centralized(DecisionModel):
+
+    
+    def retailer_profit(self):
+        pass
+
+    
+    def supplier_profit(self):
+        pass
+
+    
+    def total_profit(self):
+        pass
