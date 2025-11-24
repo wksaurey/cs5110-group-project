66da4987d82e0672188025531217100f67752e36
Rox <roxseeley@gmail.com>
2025-11-24 11:38:22 -0700

    added in helper functions for different values

--- CentralizedModel.py
+++ CentralizedModel.py
@@ -14,6 +14,38 @@ class Centralized(DecisionModel):
     D: float = 100       # base market demand
     C: float = 60        # unit cost
     
+    #helper functions
+    def phi1(self, v):
+        #φ₁(t) = φ₀ + λ₁ v
+        return self.phi0 + self.lambda1 * v
+
+    def phi(self, v):
+        #φ(t) = φ₁(t) − τ t² / T²
+        return self.phi1(v) - self.tau * (self.t**2) / (self.T**2)
+
+    def cost_preservation(self, v):
+        #Cᵥ = λ₂ v
+        return self.lambda2 * v
+
+    def D1(self, P1, P2, v):
+        #Demand for retailer
+        return (
+            self.mu * self.D
+            - self.a * P1
+            + self.b * P2
+            + self.lambda3 * self.phi(v)
+        )
+
+    def D2(self, P1, P2, v):
+        #Demand for supplier
+        return (
+            (1 - self.mu) * self.D
+            - self.a * P2
+            + self.b * P1
+            + self.lambda3 * self.phi(v)
+        )
+
+
     def retailer_profit(self, P1, P2, v):
         #pi₁ = (P1 − C) D1
         return (P1 - self.C) * self.D1(P1, P2, v)
