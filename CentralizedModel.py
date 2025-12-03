8ccebd4269b3603ca0e145cacf138d90282ca444
Rox <roxseeley@gmail.com>
2025-12-02 17:52:25 -0700

    added the optimized equations

--- CentralizedModel.py
+++ CentralizedModel.py
@@ -1,4 +1,4 @@
-import DecisionModel
+from DecisionModel import DecisionModel
 
 class Centralized(DecisionModel):
     phi0: float = 1        # φ0
@@ -14,7 +14,8 @@ class Centralized(DecisionModel):
     D: float = 100       # base market demand
     C: float = 60        # unit cost
     
-    #helper functions
+    # made these and realized I didn't need them for optimized
+    # keeping them for now just in case
     def phi1(self, v):
         #φ₁(t) = φ₀ + λ₁ v
         return self.phi0 + self.lambda1 * v
@@ -45,7 +46,6 @@ class Centralized(DecisionModel):
             + self.lambda3 * self.phi(v)
         )
 
-
     def retailer_profit(self, P1, P2, v):
         #pi₁ = (P1 − C) D1
         return (P1 - self.C) * self.D1(P1, P2, v)
@@ -62,3 +62,66 @@ class Centralized(DecisionModel):
             + (P2 - self.C) * self.D2(P1, P2, v)
             - self.cost_preservation(v)
         )
+    
+# optized equations
+    def optimal_retailer(self):
+        return (self.lambda2 / (2 * self.lambda1 * self.lambda3)
+                - (1 - 2 * self.mu) * self.D / (4 * (self.a + self.b))
+                + self.C)
+
+    def optimal_supplier(self):
+        return (self.lambda2 / (2 * self.lambda1 * self.lambda3)
+                + (1 - 2 * self.mu) * self.D / (4 * (self.a + self.b))
+                + self.C)
+
+    def optimal_v(self):
+        # optimal preservation
+        P1 = self.optimal_retailer()
+        P2 = self.optimal_supplier()
+
+        numerator = (
+            self.lambda3 * self.tau * self.t**2 / self.T**2
+            + 2*self.a*P1
+            - 2*self.b*P2
+            - self.mu*self.D
+            - (self.a - self.b)*self.C
+            - self.lambda3*self.phi0
+        )
+        return numerator / (self.lambda1 * self.lambda3)
+
+    def optimal_D1(self):
+        # demand for supplier
+        return ((self.a - self.b)*self.lambda2
+                / (2*self.lambda1*self.lambda3)
+                - (1 - 2*self.mu)*self.D / 4)
+
+    def optimal_D2(self):
+        return ((self.a - self.b)*self.lambda2
+                / (2*self.lambda1*self.lambda3)
+                + (1 - 2*self.mu)*self.D / 4)
+
+    def optimal_profit(self):
+        term1 = ((1 - 2*self.mu)*self.D)**2 / (8*(self.a + self.b))
+
+        term2 = -(self.a - self.b)*(self.lambda2**2) / (2*(self.lambda1*self.lambda3)**2)
+
+        term3 = (
+            self.lambda2 * (
+                self.D*self.T**2
+                - 2*(self.a - self.b)*self.C*self.T**2
+                + 2*self.lambda3*(self.phi0*self.T**2 - self.t**2*self.tau)
+            )
+        ) / (2*self.lambda1*self.lambda3*self.T**2)
+
+        return term1 + term2 + term3
+    
+    def summary(self):
+        return {
+            "P1*": self.optimal_retailer(),
+            "P2*": self.optimal_supplier(),
+            "v*": self.optimal_v(),
+            "profit*": self.optimal_profit()
+        }
+
+model = Centralized()
+print(model.summary())
