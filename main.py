60dc809c9a53bbcb832ccb2d727566163a4ca59d
Kolter Saurey <0396@asirobots.com>
2025-12-09 00:58:48 -0700

    reformat output

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from CentralizedModel import Centralized
 from StackelbergModel import StackelbergModel
+from Cournot import Cournot
 
-print("----Centralized Model----")
-centralized = Centralized()
-stats = centralized.summary()
-for item in stats:
-    print(item, stats[item])
-print("\n")
+def main():
+    print("----Centralized Model----")
+    centralized = Centralized()
+    stats = centralized.summary()
+    print_items(stats)
+    print("\n")
 
-print("----Stackelberg Model----")
-stackelbergModel = StackelbergModel()
-stats = stackelbergModel.summary()
-for item in stats:
-    print(item, stats[item])
-print("\n")
+    print("----Stackelberg Model----")
+    stackelbergModel = StackelbergModel()
+    stats = stackelbergModel.summary()
+    print_items(stats)
+    print("\n")
 
+    print("----Cournot Model----")
+    cournot = Cournot()
+    stats = cournot.summary()
+    print_items(stats)
 
-print("\n")
-print("----Cournot Model----")
+def print_items(stats):
+    for item in stats:
+        print(f'{item.ljust(8)} {round(stats[item], 2)}')
+
+main()
